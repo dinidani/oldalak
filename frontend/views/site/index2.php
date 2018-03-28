10d7df91ae506aa99ef56fbea2b6aa17326f12b7
www-data <www-data@debian>
2018-03-28 13:03:49 +0200

    link javitas

--- frontend/views/site/index2.php
+++ frontend/views/site/index2.php
@@ -6,7 +6,7 @@ $this->title = 'Oldalak Alkamlazás';
 <div class="site-index">
 
     <div class="jumbotron">
-        <p><a class="btn btn-lg btn-success" href="/oldalak/frontend/web/index.php?r=oldalnezet%2Findex">Oldalak Megtekintése</a></p>
+        <p><a class="btn btn-lg btn-success" href="<?php echo Yii::getAlias('@web')?>/index.php?r=oldalnezet%2Findex">Oldalak Megtekintése</a></p>
     </div>
 
 
